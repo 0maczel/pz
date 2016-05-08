01237edf9be2bd7a6ac698064c1c4b59392b4c17
Dawid Komorowski <dawidkomorowski@outlook.com>
2016-05-08 22:13:11 +0200

    Dodany helper do tworzenia URI zasobów.

---
+++ monitor/monitor-server/src/main/java/pz/monitor/service/common/UriHelper.java
@@ -0,0 +1,50 @@
+package pz.monitor.service.common;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import pz.monitor.db.entity.Entity;
+
+public class UriHelper {
+	public static String basePath() {
+		return ServletUriComponentsBuilder.fromCurrentContextPath().toUriString();
+	}
+
+	public static String currentPath() {
+		return ServletUriComponentsBuilder.fromCurrentRequestUri().toUriString();
+	}
+
+	public static String resourcePath(Long id) {
+		return ServletUriComponentsBuilder.fromCurrentRequestUri().path("/" + id).toUriString();
+	}
+
+	public static <T extends Entity> String resourcePath(T entity) {
+		return resourcePath(entity.getId());
+	}
+
+	public static <T extends Entity> List<String> resourcePath(List<T> entities) {
+		List<String> resources = new ArrayList<>();
+		for (T entity : entities) {
+			resources.add(resourcePath(entity));
+		}
+		return resources;
+	}
+
+	public static String resourcePath(String resourceMapping, Long id) {
+		return ServletUriComponentsBuilder.fromCurrentContextPath().path(resourceMapping).path("/" + id).toUriString();
+	}
+
+	public static <T extends Entity> String resourcePath(String resourceMapping, T entity) {
+		return resourcePath(resourceMapping, entity.getId());
+	}
+
+	public static <T extends Entity> List<String> resourcePath(String resourceMapping, List<T> entities) {
+		List<String> resources = new ArrayList<>();
+		for (T entity : entities) {
+			resources.add(resourcePath(resourceMapping, entity));
+		}
+		return resources;
+	}
+}
--- monitor/monitor-server/src/main/java/pz/monitor/service/measurement/MeasurementService.java
+++ monitor/monitor-server/src/main/java/pz/monitor/service/measurement/MeasurementService.java
@@ -1,5 +1,7 @@
 package pz.monitor.service.measurement;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,30 +10,44 @@ import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import pz.monitor.db.Repository;
 import pz.monitor.db.entity.Measurement;
+import pz.monitor.service.common.UriHelper;
 
 @RestController
 @Transactional
 public class MeasurementService {
 	@Autowired
 	Repository repository;
-	
+
+	@RequestMapping(path = "/measurements", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
+	public List<String> get(@RequestParam(name = "id", required = false, defaultValue="0") int id) {
+		List<Measurement> resources = new ArrayList<>();
+		
+		if(id != 0) {	
+			resources.add(repository.get(Measurement.class, new Long(id)));
+		}
+		else {
+			resources = repository.all(Measurement.class);
+		}
+		return UriHelper.resourcePath(resources);
+	}
+
 	@RequestMapping(path = "/measurements/{id}", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
-    public Measurement get(@PathVariable Long id)
-    {
-        return repository.get(Measurement.class, id);
-    }
-	
+	public Measurement get(@PathVariable Long id) {
+		return repository.get(Measurement.class, id);
+	}
+
 	@RequestMapping(path = "/measurements/random", produces = MediaType.APPLICATION_JSON_VALUE, method = RequestMethod.GET)
-    public Measurement newRandom()
-    {
+	public String newRandom() {
 		Measurement m = new Measurement();
 		Random r = new Random();
 		m.setValue(r.nextDouble());
-        repository.save(m);
-        return m;
-    }
+		repository.save(m);
+		return UriHelper.resourcePath("/measurements", m);
+	}
+
 }
