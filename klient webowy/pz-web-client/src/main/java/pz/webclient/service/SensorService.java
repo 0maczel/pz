b246945c2d7319d9d55fcecf66afe328b4e38878
Mateusz Nawrot <mateusz@softnauts.com>
2016-05-16 20:27:17 +0200

    added implementation for sensorservice methods

--- klient webowy/pz-web-client/src/main/java/pz/webclient/service/SensorService.java
+++ klient webowy/pz-web-client/src/main/java/pz/webclient/service/SensorService.java
@@ -1,18 +1,44 @@
 package pz.webclient.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 import pz.monitor.service.sensor.SensorDto;
 
+import java.net.URI;
 import java.util.List;
 
 @Service
 public class SensorService {
 
-    public List<SensorDto> getSensor(String resourceName, String metricName) {
-        return null;
-    }
+    @Value("${apiEndpoint}")
+    String endpoint;
 
+
+    @SuppressWarnings("unchecked")
+    public List<SensorDto> getSensor(String resourceName, String metricName) {
+        RestTemplate template = new RestTemplate();
+
+        URI targetUri = UriComponentsBuilder.
+                fromUriString(endpoint).
+                path("/sensors").
+                queryParam("resource-name", resourceName).
+                queryParam("metric-name", metricName).
+                build().toUri();
+
+        return template.getForObject(targetUri, List.class);
+
+    }
     public SensorDto getSensorById(Long id) {
-        return null;
+        RestTemplate template = new RestTemplate();
+
+        URI targetUri = UriComponentsBuilder.
+                fromUriString(endpoint).
+                path("/sensors/").
+                pathSegment(String.valueOf(id)).
+                build().toUri();
+
+        return template.getForObject(targetUri, SensorDto.class);
     }
 }
