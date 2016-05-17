0e9f955bc520ceae5a3e7c2a07e131d0d44aa237
Mateusz Nawrot <mnawrot@infusion.com>
2016-05-17 11:13:33 +0200

    added implementation for resource service

--- klient webowy/pz-web-client/src/main/java/pz/webclient/service/ResourceService.java
+++ klient webowy/pz-web-client/src/main/java/pz/webclient/service/ResourceService.java
@@ -1,22 +1,53 @@
 package pz.webclient.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 import pz.monitor.service.resource.ResourceDto;
 
+import java.net.URI;
 import java.util.List;
 
 @Service
 public class ResourceService {
 
+    @Value("${apiEndpoint}")
+    String endpoint;
+
+    @SuppressWarnings("unchecked")
     public List<ResourceDto> getResourceByName(String name) {
-        return null;
+        RestTemplate template = new RestTemplate();
+
+        URI targetUri = UriComponentsBuilder.
+                fromUriString(endpoint).
+                path("/resources").
+                queryParam("name-like", name).
+                build().toUri();
+
+        return template.getForObject(targetUri, List.class);
     }
 
     public ResourceDto getResourceById(Long id) {
-        return null;
+        RestTemplate template = new RestTemplate();
+
+        URI targetUri = UriComponentsBuilder.
+                fromUriString(endpoint).
+                path("/resources").
+                pathSegment("/" + String.valueOf(id)).
+                build().toUri();
+
+        return template.getForObject(targetUri, ResourceDto.class);
     }
 
     public ResourceDto createResource(ResourceDto dto) {
-        return null;
+        RestTemplate template = new RestTemplate();
+
+        URI targetUri = UriComponentsBuilder.
+                fromUriString(endpoint).
+                path("/resources").
+                build().toUri();
+
+        return template.postForObject(targetUri, dto, ResourceDto.class);
     }
 }
