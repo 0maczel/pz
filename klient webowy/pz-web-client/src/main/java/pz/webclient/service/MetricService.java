5de6abd1f7e01cdcd2090b04925715a931c98481
Mateusz Nawrot <mnawrot@infusion.com>
2016-05-17 11:30:31 +0200

    added implementation for metricservice

--- klient webowy/pz-web-client/src/main/java/pz/webclient/service/MetricService.java
+++ klient webowy/pz-web-client/src/main/java/pz/webclient/service/MetricService.java
@@ -1,22 +1,57 @@
 package pz.webclient.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.util.StringUtils;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 import pz.monitor.service.metric.MetricDto;
 
+import java.net.URI;
 import java.util.List;
 
 @Service
 public class MetricService {
 
+    @Value("${apiEndpoint}")
+    String endpoint;
+
+    @SuppressWarnings("unchecked")
     public List<MetricDto> getMetricByName(String name) {
-        return null;
+        RestTemplate template = new RestTemplate();
+
+        UriComponentsBuilder builder = UriComponentsBuilder.
+                fromUriString(endpoint).
+                path("/metrics");
+
+        if (!StringUtils.isEmpty(name))
+            builder.queryParam("name-like", name);
+
+        URI targetUri = builder.build().toUri();
+
+        return template.getForObject(targetUri, List.class);
     }
 
     public MetricDto getMetricById(Long id) {
-        return null;
+        RestTemplate template = new RestTemplate();
+
+        URI targetUri = UriComponentsBuilder.
+                fromUriString(endpoint).
+                path("/metrics").
+                pathSegment("/" + String.valueOf(id)).
+                build().toUri();
+
+        return template.getForObject(targetUri, MetricDto.class);
     }
 
     public MetricDto createMetric(MetricDto dto) {
-        return null;
+        RestTemplate template = new RestTemplate();
+
+        URI targetUri = UriComponentsBuilder.
+                fromUriString(endpoint).
+                path("/metrics").
+                build().toUri();
+
+        return template.postForObject(targetUri, dto, MetricDto.class);
     }
 }
