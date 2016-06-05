b61603163e64f025e1068272b55204dd82566706
Dariusz Duda <darekduda21@gmail.com>
2016-06-05 18:31:10 +0200

    Ladowanie pomiarow, zasobow i metryk z podanych w konfigu monitorow

--- KlientAutomatyczny/KlientAutomatyczny/main.py
+++ KlientAutomatyczny/KlientAutomatyczny/main.py
@@ -1,5 +1,14 @@
 from config_loader import ConfigLoader
+from measurements_retriever import MeasurementsRetriever
+import datetime
 
 if __name__ == '__main__':
     config_loader = ConfigLoader()
     config_loader.parse_config()
+    monitors = config_loader.get_monitors()
+    refresh_period = config_loader.get_refresh_period()
+
+    measurements_retriever = MeasurementsRetriever(monitors)
+    begin_timestamp = datetime.datetime.utcnow() - datetime.timedelta(0, refresh_period)
+    measurements_dict = measurements_retriever.get_measurements(begin_timestamp)
+
--- KlientAutomatyczny/KlientAutomatyczny/measurements_retriever.py
+++ KlientAutomatyczny/KlientAutomatyczny/measurements_retriever.py
@@ -1,4 +1,46 @@
 import requests
+from collections import OrderedDict
+
 
 class MeasurementsRetriever(object):
-    pass
+    MEASUREMENTS_POSTFIX = '/measurements'
+    RESOURCES_POSTFIX = '/resources'
+    METRICS_POSTFIX = '/metrics'
+
+    def __init__(self, monitors_dict):
+        self._monitors_names = monitors_dict.keys()
+        self._monitors_ips = monitors_dict.values()
+
+    def get_measurements(self, begin_measurements_datetime):
+        begin_timestamp_str = begin_measurements_datetime.strftime('%Y-%m-%d %H:%M:%S GMT')
+        tmp_measurements = self._retrieve_last_measurements(begin_timestamp_str)
+        tmp_metrics = self._retrieve_metrics()
+        tmp_resources = self._retrieve_resources()
+        
+
+    def _retrieve_last_measurements(self, begin_timestamp_str):
+        temporary_measurements_dict = OrderedDict()
+        for i, monitor_ip in enumerate(self._monitors_ips):
+            measurements_request = requests.get(monitor_ip + self.MEASUREMENTS_POSTFIX, params={'from-date': begin_timestamp_str})
+            temporary_measurements_dict[self._monitors_names[i]] = measurements_request.json()
+        return temporary_measurements_dict
+
+    def _retrieve_metrics(self):
+        temporary_metrics_dict = OrderedDict()
+        for i, monitor_ip in enumerate(self._monitors_ips):
+            measurements_request = requests.get(monitor_ip + self.METRICS_POSTFIX)
+            temporary_metrics_dict[self._monitors_names[i]] = OrderedDict()
+            metrics_definition_list = measurements_request.json()
+            for metric_definition in metrics_definition_list:
+                temporary_metrics_dict[self._monitors_names[i]][metric_definition['id']] = metric_definition['name']
+        return temporary_metrics_dict
+
+    def _retrieve_resources(self):
+        temporary_resources_dict = OrderedDict()
+        for i, monitor_ip in enumerate(self._monitors_ips):
+            resources_request = requests.get(monitor_ip + self.RESOURCES_POSTFIX)
+            temporary_resources_dict[self._monitors_names[i]] = OrderedDict()
+            resources_definition_list = resources_request.json()
+            for resource_definition in resources_definition_list:
+                temporary_resources_dict[self._monitors_names[i]][resource_definition['id']] = resource_definition['name']
+        return temporary_resources_dict
