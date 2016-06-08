5afa457a3105bbd833991314239ab591ff296c29
Mateusz Nawrot <mnawrot@infusion.com>
2016-06-08 19:33:39 +0200

    added user model

---
+++ klient webowy/pz-web-client/src/main/java/pz/webclient/authorisation/model/User.java
@@ -0,0 +1,87 @@
+package pz.webclient.authorisation.model;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+/**
+ * Created by mnawrot on 08/06/16.
+ */
+@Entity
+@Table(name = "users")
+public class User implements Serializable {
+
+    public User() {
+    }
+
+    public User(long id) {
+        this.id = id;
+    }
+
+    @Id
+    @GeneratedValue
+    private long id;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column(unique = true)
+    private String email;
+
+    @Column
+    private String password;
+
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
